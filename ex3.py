b4ac49d91b7ff413fd1708a2dff5baa9d1455f68
Tim Bennett <bennett.tim@gmail.com>
2017-02-16 05:09:27 +1100

    began writing next stage - gathering temperatures

--- ex3.py
+++ ex3.py
@@ -44,7 +44,44 @@ for i in str_forecasts:
     print(str_days[count], ':\n\n', i, '\n')
     count += 1
 
-    
+# -- Next we need to pull out the town temperatures we want
+# These are in a table, Pandas does this better, but Im gonna stick with BS4 to learn it better.
+# Revision on prior line -- BS4 is NOT the best for this -- pandas 100%
+# Also, Sometimes there is a 'min' and 'max' column, and sometimes just a 'max'
+# I'll probably have to write up a if clause there, eg:
+# if min print min -- else pass
+# print max
+#
+# The format I want is this:
+#
+# Adelaide  Min: 18 C   Max: 28 C   Partly Cloudy
+# Kadina    Min: 16 C   Max: 29 C   Partly Cloudy
+# Kingscote Min: 16 C   Max: 22 C   Possible Morning Shower
+#
+# etc.
+##
+##t = requests.get("http://www.bom.gov.au/sa/forecasts/towns.shtml")
+##t = t.content
+##soup2 = BeautifulSoup(t, "html.parser")
+##
+##table = soup2.find_all('table')
+####print((table))
+####rows = table.find_all('tr')
+##
+##for tr in table:
+##    td = tr.find_all('td')
+##    row = [i.text for i in td]
+##    print(row)
+
+import pandas as pd
+
+dfs = pd.read_html("http://www.bom.gov.au/sa/forecasts/towns.shtml")
+for df in dfs:
+    print(df)
+
+
+
+
 
 #apparently I can't make a result set from a result set :(
 ##sub_content = content.find_all("p")
